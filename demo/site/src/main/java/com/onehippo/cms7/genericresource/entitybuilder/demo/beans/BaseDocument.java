8c7e400091a3d4f36e66c4d6ccfdd081caf10425
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-10-10 11:28:42 -0400

    Adding demo project

---
+++ demo/site/src/main/java/com/onehippo/cms7/genericresource/entitybuilder/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.onehippo.cms7.genericresource.entitybuilder.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippoaddongenericresourceentitybuilderdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
