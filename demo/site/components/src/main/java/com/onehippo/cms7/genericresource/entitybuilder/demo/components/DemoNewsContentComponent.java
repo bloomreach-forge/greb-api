ce834933bf9b28569a213db1431757832dfd021f
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-25 22:54:25 +0900

    SERVPLUG-57: applying module to demo

---
+++ demo/site/components/src/main/java/com/onehippo/cms7/genericresource/entitybuilder/demo/components/DemoNewsContentComponent.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ */
+package com.onehippo.cms7.genericresource.entitybuilder.demo.components;
+
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.component.HstURL;
+import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
+
+import com.onehippo.cms7.genericresource.entitybuilder.GenericResourceEntityBuilder;
+import com.onehippo.cms7.genericresource.entitybuilder.demo.beans.NewsDocument;
+
+public class DemoNewsContentComponent extends EssentialsContentComponent {
+
+    @Override
+    public void doBeforeRender(final HstRequest request, final HstResponse response) {
+        super.doBeforeRender(request, response);
+
+        contributeResourceEntities(request, response);
+    }
+
+    private void contributeResourceEntities(final HstRequest request, final HstResponse response) {
+        GenericResourceEntityBuilder builder = GenericResourceEntityBuilder.get(request.getRequestContext());
+
+        final NewsDocument document = (NewsDocument) request.getRequestContext().getContentBean();
+
+        if (document != null) {
+            builder.setResourceEntity("document", document);
+        }
+
+        final HstURL componentRenderingURL = response.createComponentRenderingURL();
+        builder.setResourceEntity("componentRenderingURL", componentRenderingURL);
+    }
+}
