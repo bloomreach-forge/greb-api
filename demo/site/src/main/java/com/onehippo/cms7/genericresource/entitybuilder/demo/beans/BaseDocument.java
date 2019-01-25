dac1ecd63b0b565c0df85b66de5f930b7267bb3e
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-25 21:10:09 +0900

    SERVPLUG-57: removing old demo

--- demo/site/src/main/java/com/onehippo/cms7/genericresource/entitybuilder/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package com.onehippo.cms7.genericresource.entitybuilder.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="hippoaddongenericresourceentitybuilderdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
